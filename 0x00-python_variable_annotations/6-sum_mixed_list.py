a61562a510d1c9d0de9ccf7d0d71158d9e57e3cb
jzamora5 <jzamora_5@yahoo.com>
2020-10-19 12:13:30 -0500

    Remove unnecesary imports

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Complex types - mixed list """
-from typing import Callable, Iterator, Union, Optional, List
+from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
