e5fe0686e459218acea30c1b8fa471b99adbad12
jzamora5 <jzamora_5@yahoo.com>
2020-10-19 12:19:42 -0500

    Function docstring in one line

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,8 +4,6 @@ from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """ Takes a list mxd_lst of floats and integers
-    returns their sum as a float.
-    """
+    """ returns their sum as a float"""
 
     return float(sum(mxd_lst))
