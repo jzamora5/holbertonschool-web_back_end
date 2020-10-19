ada1bb8d78e9722a4baf7fccdf17c0589daa7604
jzamora5 <jzamora_5@yahoo.com>
2020-10-19 12:29:49 -0500

    Rewrite docstrings

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-"""
-Complex types - mixed list
-
-"""
-
+""" mixed list """
 from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """ returns their sum as a float"""
+    """  returns sum as a float. """
     return float(sum(mxd_lst))
