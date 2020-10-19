f2f725ef2a2c8e1e07f3696bd125f2d2ee74cf89
jzamora5 <jzamora_5@yahoo.com>
2020-10-19 10:51:03 -0500

    Fix docstring

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,8 +5,8 @@ from typing import Callable, Iterator, Union, Optional, List, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Takes a list mxd_lst of floats and integers
-    returns their sum as a float.
+    takes a string k and an int OR float v as arguments
+    returns a tuple.
     """
 
     return (k, v**2)
