84fcfc497f49e1eb85b7a5685a4b76b0b43489f8
jzamora5 <jzamora_5@yahoo.com>
2020-10-20 12:31:42 -0500

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import Awaitable
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """ Tasks """
+    t = asyncio.create_task(wait_random(max_delay))
+    return t
