5a5d36c1f8d9d2adf7c935cf49427008e67f61ef
jzamora5 <jzamora_5@yahoo.com>
2020-10-20 12:33:09 -0500

    Change typing

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import asyncio
-from typing import Awaitable
+from asyncio import Task, create_task
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Task:
     """ Tasks """
-    t = asyncio.create_task(wait_random(max_delay))
+    t = create_task(wait_random(max_delay))
     return t
