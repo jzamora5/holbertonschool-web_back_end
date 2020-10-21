5008442482de015d91c5238f7081341c22a5c1b9
jzamora5 <jzamora_5@yahoo.com>
2020-10-21 12:08:09 -0500

    Change generator typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 from asyncio import sleep
 from random import uniform
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async Generator """
     for _ in range(10):
         await sleep(1)
