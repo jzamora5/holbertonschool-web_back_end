83397b528df8e1076cac827b9a00d7ff9b12c06f
jzamora5 <jzamora_5@yahoo.com>
2020-10-30 10:15:21 -0500

    Regex-ing

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Handling Personal Data
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
