6f4547bad31e6a3595a2d9d062d34a0d89dacde9
jzamora5 <jzamora_5@yahoo.com>
2020-10-30 13:35:44 -0500

    Connect to secure database

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 Handling Personal Data
 """
 import logging
+import mysql.connector
+from os import environ
 import re
 from typing import List
 
@@ -30,6 +32,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username, password=password,
+                                                     host=host, database=db_name)
+    return cnx
 
 
 class RedactingFormatter(logging.Formatter):
