0860b6f7afffe0b3c31444031824b0ac7b3a8cdb
jzamora5 <jzamora_5@yahoo.com>
2020-11-09 20:14:28 -0500

    Sessions in database

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -34,12 +34,11 @@ class SessionAuth(Auth):
         """Returns a User instance based on a cookie value"""
 
         session_id = self.session_cookie(request)
-        print(session_id)
+
         if session_id is None:
             return None
 
         user_id = self.user_id_for_session_id(session_id)
-        print(user_id)
 
         return User.get(user_id)
 
@@ -58,6 +57,9 @@ class SessionAuth(Auth):
         if not user_id:
             return False
 
-        del self.user_id_by_session_id[session_id]
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
 
         return True
