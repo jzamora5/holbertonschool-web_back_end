65a6d7f64414554a85d2145adf2491c5b7f79d7a
jzamora5 <jzamora_5@yahoo.com>
2020-11-16 08:41:51 -0500

    Add extra validation Update_password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -112,6 +112,9 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Uses reset token to validate update of users password"""
+        if reset_token is None or password is None:
+            return None
+
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
