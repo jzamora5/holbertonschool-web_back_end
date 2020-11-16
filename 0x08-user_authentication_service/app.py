af0748be8db2538c48486208f8019b410a2fa88a
jzamora5 <jzamora_5@yahoo.com>
2020-11-16 09:13:15 -0500

    Better docs for update password

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -120,17 +120,19 @@ def reset_password() -> str:
 
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
-    """Update the password. If the token is invalid,
-    catch the exception and respond with a 403 HTTP code.
-
-    If the token is valid, respond with a 200 HTTP code and the
-    following JSON payload:
-    {"email": "<user email>", "message": "Password updated"}
+    """ PUT /reset_password
+    Updates password with reset token
+    Return:
+        - 400 if bad request
+        - 403 if not valid reset token
+        - 200 and JSON Payload if valid
     """
-
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
+    try:
+        email = request.form('email')
+        reset_token = request.form('reset_token')
+        new_password = request.form('new_password')
+    except:
+        abort(400)
 
     try:
         AUTH.update_password(reset_token, new_password)
