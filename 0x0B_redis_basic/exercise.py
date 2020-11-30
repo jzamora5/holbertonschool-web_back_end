5e9db81666d391b1799ed798f256da66d462d169
jzamora5 <jzamora_5@yahoo.com>
2020-11-30 15:44:07 -0500

    Redis Basic Typo Correction

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,7 +20,7 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """ decorator to store the history of inputs and
+    """ Decorator to store the history of inputs and
     outputs for a particular function.
     """
 
