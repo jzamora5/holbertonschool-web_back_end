cc629815de572169491448de78a12bfbba6dc6cd
jzamora5 <jzamora_5@yahoo.com>
2020-12-01 07:45:46 -0500

    Change incr

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,7 +13,7 @@ def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """ Wrapper for decorator functionality """
-        self._redis.incr(method.__qualname__, amount=1)
+        self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
 
     return wrapper
