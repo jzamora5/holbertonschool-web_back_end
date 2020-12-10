df299bfe76dff614554e8cf1c33f886c0bb0c9d6
jzamora5 <jzamora_5@yahoo.com>
2020-12-09 19:19:54 -0500

    Remove callback

--- 0x0E-ES6_basic/compiler.js
+++ 0x0E-ES6_basic/compiler.js
@@ -1,20 +1,7 @@
 function compiler(input, output) {
   const { exec, execSync } = require('child_process');
 
-  execSync(
-    `npx babel ${input} --out-file ${output}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      // console.log(`stdout: ${stdout}`);
-    }
-  );
+  execSync(`npx babel ${input} --out-file ${output}`);
 }
 
 module.exports = compiler;
