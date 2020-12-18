7c6ce37626e4ee745e15fa96d240709ea8522cde
jzamora5 <jzamora_5@yahoo.com>
2020-12-18 09:21:46 -0500

    Methods

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // code
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  // name
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
