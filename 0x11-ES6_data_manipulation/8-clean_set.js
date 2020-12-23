b8b236e21c81130bbb4495f016e3352e9fad9c54
jzamora5 <jzamora_5@yahoo.com>
2020-12-23 08:03:42 -0500

    Try check item

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,15 +2,15 @@ function cleanSet(set, startString) {
   const string = [];
 
   if (
-    typeof set !== 'object' ||
-    typeof startString !== 'string' ||
-    startString.length === 0
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
   ) {
     return '';
   }
 
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (item && item.startsWith(startString)) {
       string.push(item.slice(startString.length));
     }
   }
