5a8c15b654b2e24c56f20f9b39a69886157e7911
jzamora5 <jzamora_5@yahoo.com>
2021-01-04 12:08:38 -0500

    Express More complex server

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(args[0]);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
