0734a21eb2aa2e81a55219a3ed3bd0597626695d
jzamora5 <jzamora_5@yahoo.com>
2021-01-07 18:01:35 -0500

    Config for mocha es6 and fix test

--- 0x12-Node_JS_basic/test/students.test.js
+++ 0x12-Node_JS_basic/test/students.test.js
@@ -1,18 +1,17 @@
-const app = require('../full_server/server.js');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+import app from '../full_server/server.js';
+import { use, request, expect } from 'chai';
+import chaiHttp from 'chai-http';
 process.argv[2] = './database.csv';
 
-chai.use(chaiHttp);
+use(chaiHttp);
 
 describe('Server!', () => {
   it('welcomes user to the api', (done) => {
-    chai
-      .request(app)
+    request(app)
       .get('/students/CS')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body.message).to.equals(
+        expect(res.text).to.equals(
           'List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie'
         );
         done();
