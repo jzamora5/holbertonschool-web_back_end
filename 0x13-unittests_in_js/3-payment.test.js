7dc64a3acadc3afe304d54a0b0069deccb39575b
jzamora5 <jzamora_5@yahoo.com>
2021-01-11 10:32:06 -0500

    Sinon Spies

---
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+it('has the same math', () => {
+  const spyUtils = sinon.spy(Utils, 'calculateNumber');
+  const spyConsole = sinon.spy(console, 'log');
+
+  sendPaymentRequestToApi(100, 20);
+
+  expect(spyUtils.calledOnceWith('SUM', 100, 20)).to.be.true;
+  expect(spyConsole.calledOnceWith('The total is: 120')).to.be.true;
+
+  spyUtils.restore();
+  spyConsole.restore();
+});
