7e76136fd021085e5f39783c752df14a9da8414c
jzamora5 <jzamora_5@yahoo.com>
2021-01-12 12:06:22 -0500

    String Equal

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,25 +4,25 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
   describe('Two Integers', function () {
     it('should return 4', function () {
-      assert.equal(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
   });
 
   describe('One round', function () {
     it('should return 5', function () {
-      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
   });
 
   describe('Two round', function () {
     it('should return 5', function () {
-      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
   });
 
   describe('Two round again', function () {
     it('should return 6', function () {
-      assert.equal(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
   });
 });
