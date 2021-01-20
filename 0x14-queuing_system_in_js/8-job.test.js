fd6dd816f7b41b3bb04058b29e095fd3712fea5c
jzamora5 <jzamora_5@yahoo.com>
2021-01-20 14:05:19 -0500

    ADD tests

--- 0x14-queuing_system_in_js/8-job.test.js
+++ 0x14-queuing_system_in_js/8-job.test.js
@@ -24,9 +24,40 @@ describe('createPushNotificationsJobs', () => {
     queue.testMode.exit();
   });
 
-  it('should create jobs', () => {
-    createPushNotificationsJobs(jobs, queue);
-    console.log(queue.testMode);
+  it('display a error message if jobs is not an array passing Number', () => {
+    expect(() => {
+      createPushNotificationsJobs(2, queue);
+    }).to.throw('Jobs is not an array');
     // expect(queue.testMode.job[0]);
+  });
+
+  it('display a error message if jobs is not an array passing Object', () => {
+    expect(() => {
+      createPushNotificationsJobs({}, queue);
+    }).to.throw('Jobs is not an array');
+    // expect(queue.testMode.job[0]);
+  });
+
+  it('display a error message if jobs is not an array passing String', () => {
+    expect(() => {
+      createPushNotificationsJobs('Hello', queue);
+    }).to.throw('Jobs is not an array');
+    // expect(queue.testMode.job[0]);
+  });
+
+  it('should NOT display a error message if jobs is an array with empty array', () => {
+    const ret = createPushNotificationsJobs([], queue);
+    expect(ret).to.equal(undefined);
+    // expect(queue.testMode.job[0]);
+  });
+
+  it('create two new jobs to the queue', () => {
+    queue.createJob('myJob', { foo: 'bar' }).save();
+    queue.createJob('anotherJob', { baz: 'bip' }).save();
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('myJob');
+    expect(queue.testMode.jobs[0].data).to.eql({ foo: 'bar' });
+    expect(queue.testMode.jobs[1].type).to.equal('anotherJob');
+    expect(queue.testMode.jobs[1].data).to.eql({ baz: 'bip' });
   });
 });
